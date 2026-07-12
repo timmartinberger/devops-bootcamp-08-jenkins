ad3647d9095237c57d43043c8a5cbb1ffbcc00f8
Tim Martin Berger <timmartinberger@gmail.com>
2026-07-12 20:44:04 +0200

    Execute from 01-java-maven-app

--- 01-java-maven-app/script.groovy
+++ 01-java-maven-app/script.groovy
@@ -1,3 +1,12 @@
+/* NOTE: For the bootcamp I decided to put all projects of the same module into one repo in order 
+ *      to keep similar things together and to not overcrowd my github account. Thats whyy some steps
+ *      require  something like 'cd 01-java-maven-app' before the actual command is executed.
+ *       
+ *      In a real project the Jenkinsfile and Dockerfile would be at root level of the repo
+ *      so that this would not be necessary.
+ */
+
+
 def buildJar() {
     echo 'Building the application...'
     sh 'cd 01-java-maven-app && mvn package'
