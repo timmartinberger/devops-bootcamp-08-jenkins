8ba6e2fd56b37125e9e9f632a607106bd325401b
Tim Martin Berger <timmartinberger@gmail.com>
2026-07-17 19:09:51 +0200

    Use shared library

--- 01-java-maven-app/script.groovy
+++ 01-java-maven-app/script.groovy
@@ -10,20 +10,6 @@ def testApp() {
     echo "Testing the application..."
 }
 
-def buildJar() {
-    echo 'Building the application...'
-    sh 'cd 01-java-maven-app && mvn package'
-}
-
-def buildImage() {
-    echo 'Building the application...'
-    withCredentials([usernamePassword(credentialsId: 'docker.hub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'cd 01-java-maven-app && docker build -t aarontimberg/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push aarontimberg/demo-app:jma-2.0'
-    }
-}
-
 def deployApp(){
     echo 'Deploying docker image...'
 }
